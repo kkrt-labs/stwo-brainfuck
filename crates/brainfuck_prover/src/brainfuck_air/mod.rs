dad759f17263285f163545c83babe0c5c98e4df4
malatrax <simon.malatrait@grenoble-inp.org>
2024-11-07 17:27:11 +0100

    feat: add custom error for verify_brainfuck

--- crates/brainfuck_prover/src/brainfuck_air/mod.rs
+++ crates/brainfuck_prover/src/brainfuck_air/mod.rs
@@ -10,6 +10,21 @@ use stwo_prover::core::{
         ops::MerkleHasher,
     },
 };
+use thiserror::Error;
+
+/// Represents errors that can occur when verifying
+/// a Brainfuck proof.
+#[derive(Error, Debug)]
+pub enum BrainfuckVerificationError {
+    /// Error variant indicating that the sum from the LogUp protocol
+    /// is not valid compared to the given claims.
+    #[error("Invalid lookup sum")]
+    InvalidLogupSum,
+    /// Error variant indicating that something went wrong during
+    /// the CSTARK verification `stwo_prover::prover::verify()`.
+    #[error("Stark verification error: {0}")]
+    Stark(#[from] VerificationError),
+}
 
 /// The STARK proof of the execution of a given Brainfuck program.
 ///
@@ -162,7 +177,7 @@ pub fn prove_brainfuck() -> Result<BrainfuckProof<Blake2sMerkleHasher>, ProvingE
 /// Verify a given STARK proof of a Brainfuck program execution with corresponding claim.
 pub fn verify_brainfuck(
     BrainfuckProof { claim, interaction_claim, proof }: BrainfuckProof<Blake2sMerkleHasher>,
-) -> Result<(), VerificationError> {
+) -> Result<(), BrainfuckVerificationError> {
     // ┌──────────────────────────┐
     // │     Protocol Setup       │
     // └──────────────────────────┘
@@ -184,10 +199,9 @@ pub fn verify_brainfuck(
     let interaction_elements = BrainfuckInteractionElements::draw(channel);
     // Check that the lookup sum is valid, otherwise throw
     // TODO: panic! should be replaced by custom error
-    assert!(
-        !lookup_sum_valid(&claim, &interaction_elements, &interaction_claim),
-        "Invalid lookup sum."
-    );
+    if !lookup_sum_valid(&claim, &interaction_elements, &interaction_claim) {
+        return Err(BrainfuckVerificationError::InvalidLogupSum);
+    };
     interaction_claim.mix_into(channel);
     commitment_scheme_verifier.commit(proof.commitments[1], &sizes[1], channel);
 
@@ -205,4 +219,5 @@ pub fn verify_brainfuck(
     let components = component_builder.components();
 
     verify(&components, channel, commitment_scheme_verifier, proof)
+        .map_err(BrainfuckVerificationError::Stark)
 }
