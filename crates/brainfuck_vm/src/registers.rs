22016b1b1304b4f47ece88f119a95039b523b0cc
Thomas Coratger <thomas.coratger@gmail.com>
2024-11-07 12:15:05 +0100

    revert Debug derive for Registers

--- crates/brainfuck_vm/src/registers.rs
+++ crates/brainfuck_vm/src/registers.rs
@@ -3,7 +3,7 @@
 use num_traits::identities::Zero;
 use stwo_prover::core::fields::m31::BaseField;
 
-#[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone)]
 pub struct Registers {
     /// Clock Cycle Counter
     pub clk: BaseField,
@@ -42,6 +42,16 @@ impl Registers {
 }
 
 impl std::fmt::Display for Registers {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "clk:{}, ip:{}, ci:{}, ni:{}, mp:{}, mv:{}, mvi:{}",
+            self.clk, self.ip, self.ci, self.ni, self.mp, self.mv, self.mvi
+        )
+    }
+}
+
+impl std::fmt::Debug for Registers {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
