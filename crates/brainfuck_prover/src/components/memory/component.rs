25ddf5d3562089fcfd98d36ea488b17b95bf2536
malatrax <simon.malatrait@grenoble-inp.org>
2024-11-20 14:11:30 +0100

    doc: fix header doc references

--- crates/brainfuck_prover/src/components/memory/component.rs
+++ crates/brainfuck_prover/src/components/memory/component.rs
@@ -8,15 +8,16 @@ pub struct Claim {
 }
 
 impl Claim {
-    /// Returns the log_size of the each type of trace commited for the Memory component:
+    /// Returns the `log_size` of the each type of trace commited for the Memory component:
     /// - Preprocessed trace,
     /// - Main trace,
     /// - Interaction trace.
     ///
     /// The number of columns of each trace is known before actually evaluating them.
     /// The `log_size` is known once the main trace has been evaluated
-    /// (the log2 of the size of the [`super::table::MemoryTable`], to which we add LOG_N_LANES for
-    /// the SimdBackend)
+    /// (the log2 of the size of the [`super::table::MemoryTable`], to which we add
+    /// [`stwo_prover::core::backend::simd::m31::LOG_N_LANES`]
+    /// for the [`stwo_prover::core::backend::simd::SimdBackend`])
     ///
     /// Each element of the [`TreeVec`] is dedicated to the commitment of one type of trace.
     /// First element is for the preprocessed trace, second for the main trace and third for the
@@ -29,9 +30,9 @@ impl Claim {
         TreeVec::new(vec![trace_log_sizes])
     }
 
-    /// Mix the log size of the Memory table to the Fiat-Shamir channel,
+    /// Mix the log size of the Memory table to the Fiat-Shamir [`Channel`],
     /// to bound the channel randomness and the trace.
     pub fn mix_into(&self, channel: &mut impl Channel) {
-        channel.mix_u64(u64::from(self.log_size));
+        channel.mix_u64(self.log_size.into());
     }
 }
