ed170fa6620b0dd457c998d7710b127f231e50b1
malatrax <71888134+zmalatrax@users.noreply.github.com>
2024-11-26 17:35:44 +0100

    feat: add interaction trace evaluation (#91)
    * feat: add memory interaction claim
    
    * refactor: make idx variable name more explanatory
    
    * feat: add lookup elements for Memory component
    
    * feat: add interaction trace evaluation
    
    * test: add interaction_trace_evaluation unit tests
    
    * doc: add header documentation
    
    * refactor: remove interaciton_claim variable
    
    * doc: refactor inlined comments to header doc
    
    * feat: add memory interaction log size claim
    
    * chore: normalize all logUp occurences in comments
    
    * chore: spacing
    
    * test: declare all numerators explicitly
    
    * doc: add header doc for memory InteractionClaim field
    
    Co-authored-by: Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Thomas Coratger <60488569+tcoratger@users.noreply.github.com>

--- crates/brainfuck_prover/src/components/memory/component.rs
+++ crates/brainfuck_prover/src/components/memory/component.rs
@@ -1,5 +1,9 @@
 use super::table::MemoryColumn;
-use stwo_prover::core::{channel::Channel, pcs::TreeVec};
+use stwo_prover::core::{
+    channel::Channel,
+    fields::{qm31::SecureField, secure_column::SECURE_EXTENSION_DEGREE},
+    pcs::TreeVec,
+};
 
 /// The claim for the Memory component
 #[derive(Debug, Eq, PartialEq)]
@@ -28,7 +32,7 @@ impl Claim {
         // TODO: Add the preprocessed and interaction trace correct sizes
         let preprocessed_trace_log_sizes: Vec<u32> = vec![];
         let trace_log_sizes = vec![self.log_size; MemoryColumn::count()];
-        let interaction_trace_log_sizes: Vec<u32> = vec![];
+        let interaction_trace_log_sizes: Vec<u32> = vec![self.log_size; SECURE_EXTENSION_DEGREE];
         TreeVec::new(vec![
             preprocessed_trace_log_sizes,
             trace_log_sizes,
@@ -40,5 +44,25 @@ impl Claim {
     /// to bound the channel randomness and the trace.
     pub fn mix_into(&self, channel: &mut impl Channel) {
         channel.mix_u64(self.log_size.into());
+    }
+}
+
+/// The claim of the interaction phase 2 (with the logUp protocol).
+///
+/// The total sum is the computed sum of the logUp extension column,
+/// including the padded rows.
+/// It allows proving that the Memory main trace is a permutation
+/// of the Processor trace (which is the execution trace provided by the `brainfuck_vm`).
+#[derive(Debug, Eq, PartialEq)]
+pub struct InteractionClaim {
+    /// The computed sum of the logUp extension column, including padded rows.
+    pub claimed_sum: SecureField,
+}
+
+impl InteractionClaim {
+    /// Mix the sums from the logUp protocol into the Fiat-Shamir [`Channel`],
+    /// to bound the proof to the trace.
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        channel.mix_felts(&[self.claimed_sum]);
     }
 }
