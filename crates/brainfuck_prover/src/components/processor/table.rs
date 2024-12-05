b2e5a0173ae065df08292a7050985a683d2984cf
malatrax <simon.malatrait@grenoble-inp.org>
2024-12-05 14:22:56 +0100

    refactor: add new ProcessorTable and update tests

--- crates/brainfuck_prover/src/components/processor/table.rs
+++ crates/brainfuck_prover/src/components/processor/table.rs
@@ -1,5 +1,6 @@
 use crate::components::{ProcessorClaim, TraceColumn, TraceError, TraceEval};
 use brainfuck_vm::registers::Registers;
+use num_traits::One;
 use stwo_prover::core::{
     backend::{
         simd::{column::BaseColumn, m31::LOG_N_LANES},
@@ -8,6 +9,200 @@ use stwo_prover::core::{
     fields::m31::BaseField,
     poly::circle::{CanonicCoset, CircleEvaluation},
 };
+
+/// Represents the Processor Table, which holds the required registers
+/// for the Processor component.
+///
+/// The Processor Table ensures consistency for the part of the execution that
+/// relates to the registers of the Brainfuck virtual machine. It records all
+/// register values for each cycle that the program ran.
+///
+///
+/// To ease constraints evaluation, each row of the Processor component
+/// contains the current row and the next row in natural order.
+/// This is done to avoid having to do costly bit-reversals, as constraints
+/// are evaluated on the evaluation of the trace which is ordered in
+/// a bit-reversed manner over the circle domain once the polynomials are interpolated.
+///
+/// The preliminary work to extract the fields from the execution trace,
+/// the sorting and the padding is done through the [`ProcessorIntermediateTable`] struct.
+///
+/// Once done, we can build the Processor table from it, by pairing the consecutive
+#[derive(Debug, Default, PartialEq, Eq, Clone)]
+pub struct ProcessorTable {
+    /// A vector of [`ProcessorTableRow`] representing the table rows.
+    table: Vec<ProcessorTableRow>,
+}
+
+impl ProcessorTable {
+    /// Creates a new, empty [`ProcessorTable`].
+    ///
+    /// # Returns
+    /// A new instance of [`ProcessorTable`] with an empty table.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Adds a new row to the Memory Table.
+    ///
+    /// # Arguments
+    /// * `row` - The [`ProcessorTableRow`] to add to the table.
+    ///
+    /// This method pushes a new [`MemoryTableRow`] onto the `table` vector.
+    fn add_row(&mut self, row: ProcessorTableRow) {
+        self.table.push(row);
+    }
+
+    /// Transforms the [`ProcessorTable`] into a [`TraceEval`], to be committed when
+    /// generating a STARK proof.
+    ///
+    /// Converts the entry-based table into columnar format and evaluates it over
+    /// the circle domain.
+    ///
+    /// # Returns
+    /// A tuple containing the evaluated trace and claim for STARK proof.
+    ///
+    /// # Errors
+    /// Returns `TraceError::EmptyTrace` if the table is empty.
+    pub fn trace_evaluation(&self) -> Result<(TraceEval, ProcessorClaim), TraceError> {
+        let n_rows = self.table.len() as u32;
+        if n_rows == 0 {
+            return Err(TraceError::EmptyTrace);
+        }
+
+        // Compute log size and adjust for SIMD lanes
+        let log_n_rows = n_rows.ilog2();
+        let log_size = log_n_rows + LOG_N_LANES;
+
+        // Initialize trace columns
+        let mut trace = vec![BaseColumn::zeros(1 << log_size); ProcessorColumn::count()];
+
+        // Fill columns with table data
+        for (index, row) in self.table.iter().enumerate().take(1 << log_n_rows) {
+            trace[ProcessorColumn::Clk.index()].data[index] = row.clk.into();
+            trace[ProcessorColumn::Ip.index()].data[index] = row.ip.into();
+            trace[ProcessorColumn::Ci.index()].data[index] = row.ci.into();
+            trace[ProcessorColumn::Ni.index()].data[index] = row.ni.into();
+            trace[ProcessorColumn::Mp.index()].data[index] = row.mp.into();
+            trace[ProcessorColumn::Mv.index()].data[index] = row.mv.into();
+            trace[ProcessorColumn::Mvi.index()].data[index] = row.mvi.into();
+            trace[ProcessorColumn::NextClk.index()].data[index] = row.next_clk.into();
+            trace[ProcessorColumn::NextIp.index()].data[index] = row.next_ip.into();
+            trace[ProcessorColumn::NextCi.index()].data[index] = row.next_ci.into();
+            trace[ProcessorColumn::NextNi.index()].data[index] = row.next_ni.into();
+            trace[ProcessorColumn::NextMp.index()].data[index] = row.next_mp.into();
+            trace[ProcessorColumn::NextMv.index()].data[index] = row.next_mv.into();
+            trace[ProcessorColumn::NextMvi.index()].data[index] = row.next_mvi.into();
+        }
+
+        // Evaluate columns on the circle domain
+        let domain = CanonicCoset::new(log_size).circle_domain();
+        let trace = trace.into_iter().map(|col| CircleEvaluation::new(domain, col)).collect();
+
+        // Return the evaluated trace and claim
+        Ok((trace, ProcessorClaim::new(log_size)))
+    }
+}
+
+impl From<Vec<Registers>> for ProcessorTable {
+    fn from(registers: Vec<Registers>) -> Self {
+        ProcessorIntermediateTable::from(registers).into()
+    }
+}
+
+// Separated from `Vec<Registers> for ProcessorTable` to facilitate tests.
+// It is assumed that [`ProcessorIntermediateTable`] is sorted and padded to the next power of two.
+impl From<ProcessorIntermediateTable> for ProcessorTable {
+    fn from(mut intermediate_table: ProcessorIntermediateTable) -> Self {
+        let mut memory_table = Self::new();
+
+        if intermediate_table.table.is_empty() {
+            return memory_table;
+        }
+
+        let last_entry = intermediate_table.table.last().unwrap();
+        let next_dummy_entry =
+            ProcessorTableEntry { clk: last_entry.clk + BaseField::one(), ..last_entry.clone() };
+
+        intermediate_table.add_entry(next_dummy_entry);
+
+        for window in intermediate_table.table.windows(2) {
+            match window {
+                [entry_1, entry_2] => {
+                    let row = ProcessorTableRow::new(entry_1, entry_2);
+                    memory_table.add_row(row);
+                }
+                _ => panic!("Empty window"),
+            }
+        }
+        memory_table
+    }
+}
+
+/// Represents a single row in the Processor Table.
+///
+/// Two consecutive entries [`ProcessorTableEntry`] flattened.
+///
+/// To avoid bit-reversals when evaluating transition constraints,
+/// the two consecutives rows on which transition constraints are evaluated
+/// are flattened into a single row.
+#[derive(Debug, Default, PartialEq, Eq, Clone)]
+pub struct ProcessorTableRow {
+    /// Clock cycle counter: the current step.
+    clk: BaseField,
+    /// Instruction pointer: the pointer to the current instruction.
+    ip: BaseField,
+    /// Current instruction: the opcode at the current instruction pointer.
+    ci: BaseField,
+    /// Next instruction: the opcode that follows `ci`, or 0 if out of bounds.
+    ni: BaseField,
+    /// Memory pointer: points to the current cell in the memory array.
+    mp: BaseField,
+    /// Memory value: the value at the current memory cell.
+    mv: BaseField,
+    /// Memory value inverse: the modular inverse of `mv` on the cell values' range (over
+    /// [`BaseField`])
+    mvi: BaseField,
+    /// Next Clock cycle counter
+    next_clk: BaseField,
+    /// Next Instruction pointer
+    next_ip: BaseField,
+    /// Next Current instruction
+    next_ci: BaseField,
+    /// Next Next instruction
+    next_ni: BaseField,
+    /// Next Memory pointer
+    next_mp: BaseField,
+    /// Next Memory value
+    next_mv: BaseField,
+    /// Next Memory value inverse
+    next_mvi: BaseField,
+}
+
+impl ProcessorTableRow {
+    /// Creates a row for the [`ProcessorIntermediateTable`] which is considered 'real'.
+    ///
+    /// A 'real' row, is a row that is part of the execution trace from the Brainfuck program
+    /// execution.
+    pub const fn new(entry_1: &ProcessorTableEntry, entry_2: &ProcessorTableEntry) -> Self {
+        Self {
+            clk: entry_1.clk,
+            ip: entry_1.ip,
+            ci: entry_1.ci,
+            ni: entry_1.ni,
+            mp: entry_1.mp,
+            mv: entry_1.mv,
+            mvi: entry_1.mvi,
+            next_clk: entry_2.clk,
+            next_ip: entry_2.ip,
+            next_ci: entry_2.ci,
+            next_ni: entry_2.ni,
+            next_mp: entry_2.mp,
+            next_mv: entry_2.mv,
+            next_mvi: entry_2.mvi,
+        }
+    }
+}
 
 /// Represents the Processor Table, which records the register values for
 /// each cycle that the program ran.
@@ -66,49 +261,6 @@ impl ProcessorIntermediateTable {
                 });
             }
         }
-    }
-
-    /// Transforms the [`ProcessorIntermediateTable`] into a [`TraceEval`], to be committed when
-    /// generating a STARK proof.
-    ///
-    /// Converts the entry-based table into columnar format and evaluates it over
-    /// the circle domain.
-    ///
-    /// # Returns
-    /// A tuple containing the evaluated trace and claim for STARK proof.
-    ///
-    /// # Errors
-    /// Returns `TraceError::EmptyTrace` if the table is empty.
-    pub fn trace_evaluation(&self) -> Result<(TraceEval, ProcessorClaim), TraceError> {
-        let n_rows = self.table.len() as u32;
-        if n_rows == 0 {
-            return Err(TraceError::EmptyTrace);
-        }
-
-        // Compute log size and adjust for SIMD lanes
-        let log_n_rows = n_rows.ilog2();
-        let log_size = log_n_rows + LOG_N_LANES;
-
-        // Initialize trace columns
-        let mut trace = vec![BaseColumn::zeros(1 << log_size); ProcessorColumn::count()];
-
-        // Fill columns with table data
-        for (index, row) in self.table.iter().enumerate().take(1 << log_n_rows) {
-            trace[ProcessorColumn::Clk.index()].data[index] = row.clk.into();
-            trace[ProcessorColumn::Ip.index()].data[index] = row.ip.into();
-            trace[ProcessorColumn::Ci.index()].data[index] = row.ci.into();
-            trace[ProcessorColumn::Ni.index()].data[index] = row.ni.into();
-            trace[ProcessorColumn::Mp.index()].data[index] = row.mp.into();
-            trace[ProcessorColumn::Mv.index()].data[index] = row.mv.into();
-            trace[ProcessorColumn::Mvi.index()].data[index] = row.mvi.into();
-        }
-
-        // Evaluate columns on the circle domain
-        let domain = CanonicCoset::new(log_size).circle_domain();
-        let trace = trace.into_iter().map(|col| CircleEvaluation::new(domain, col)).collect();
-
-        // Return the evaluated trace and claim
-        Ok((trace, ProcessorClaim::new(log_size)))
     }
 }
 
@@ -255,8 +407,59 @@ mod tests {
     }
 
     #[test]
+    fn test_processor_table_row() {
+        let entry_1 = ProcessorTableEntry {
+            clk: BaseField::one(),
+            ip: BaseField::from(5),
+            ci: BaseField::from(43),
+            ni: BaseField::from(91),
+            mp: BaseField::from(2),
+            mv: BaseField::from(7),
+            mvi: BaseField::zero(),
+        };
+
+        let entry_2 = ProcessorTableEntry {
+            clk: BaseField::one(),
+            ip: BaseField::from(5),
+            ci: BaseField::from(63),
+            ni: BaseField::from(42),
+            mp: BaseField::from(2),
+            mv: BaseField::one(),
+            mvi: BaseField::zero(),
+        };
+
+        let row = ProcessorTableRow::new(&entry_1, &entry_2);
+
+        let expected_row = ProcessorTableRow {
+            clk: BaseField::one(),
+            ip: BaseField::from(5),
+            ci: BaseField::from(43),
+            ni: BaseField::from(91),
+            mp: BaseField::from(2),
+            mv: BaseField::from(7),
+            mvi: BaseField::zero(),
+            next_clk: BaseField::one(),
+            next_ip: BaseField::from(5),
+            next_ci: BaseField::from(63),
+            next_ni: BaseField::from(42),
+            next_mp: BaseField::from(2),
+            next_mv: BaseField::one(),
+            next_mvi: BaseField::zero(),
+        };
+
+        assert_eq!(row, expected_row);
+    }
+
+    #[test]
+    fn test_processor_intermediate_table_new() {
+        let processor_intermediate_table = ProcessorIntermediateTable::new();
+
+        assert!(processor_intermediate_table.table.is_empty());
+    }
+
+    #[test]
     fn test_processor_table_new() {
-        let processor_table = ProcessorIntermediateTable::new();
+        let processor_table = ProcessorTable::new();
 
         assert!(processor_table.table.is_empty());
     }
@@ -321,6 +524,7 @@ mod tests {
         assert_eq!(processor_table.table, entries,);
     }
 
+    #[allow(clippy::similar_names)]
     #[allow(clippy::too_many_lines)]
     #[test]
     fn test_processor_table_from_registers_example_program() {
@@ -336,7 +540,9 @@ mod tests {
         let trace = machine.trace();
 
         // Convert the trace to an `ProcessorIntermediateTable`
-        let processor_table: ProcessorIntermediateTable = trace.into();
+        let processor_intermediate_table: ProcessorIntermediateTable = trace.into();
+
+        let processor_table = ProcessorTable::from(processor_intermediate_table.clone());
 
         // Create the expected `ProcessorIntermediateTable`
         let process_0 = ProcessorTableEntry {
@@ -439,7 +645,7 @@ mod tests {
             mvi: BaseField::one(),
         };
 
-        let process_no_10 = ProcessorTableEntry {
+        let process_10 = ProcessorTableEntry {
             clk: BaseField::from(10),
             ip: BaseField::from(11),
             ci: BaseField::from(93),
@@ -449,7 +655,7 @@ mod tests {
             mvi: BaseField::zero(),
         };
 
-        let process_no_11 = ProcessorTableEntry {
+        let process_11 = ProcessorTableEntry {
             clk: BaseField::from(11),
             ip: BaseField::from(13),
             ci: BaseField::zero(),
@@ -459,32 +665,75 @@ mod tests {
             mvi: BaseField::zero(),
         };
 
-        let mut expected_processor_table = ProcessorIntermediateTable {
+        let dummy_12 = ProcessorTableEntry { clk: process_11.clk + BaseField::one(), ..process_11 };
+        let dummy_13 = ProcessorTableEntry { clk: dummy_12.clk + BaseField::one(), ..process_11 };
+        let dummy_14 = ProcessorTableEntry { clk: dummy_13.clk + BaseField::one(), ..process_11 };
+        let dummy_15 = ProcessorTableEntry { clk: dummy_14.clk + BaseField::one(), ..process_11 };
+        let dummy_16 = ProcessorTableEntry { clk: dummy_15.clk + BaseField::one(), ..process_11 };
+
+        let mut expected_processor_intermediate_table = ProcessorIntermediateTable {
             table: vec![
-                process_0,
-                process_1,
-                process_2,
-                process_3,
-                process_4,
-                process_5,
-                process_6,
-                process_7,
-                process_8,
-                process_9,
-                process_no_10,
-                process_no_11,
+                process_0.clone(),
+                process_1.clone(),
+                process_2.clone(),
+                process_3.clone(),
+                process_4.clone(),
+                process_5.clone(),
+                process_6.clone(),
+                process_7.clone(),
+                process_8.clone(),
+                process_9.clone(),
+                process_10.clone(),
+                process_11.clone(),
             ],
         };
 
-        expected_processor_table.pad();
+        expected_processor_intermediate_table.pad();
+
+        let mut expected_processor_table = ProcessorTable::new();
+
+        let row_0 = ProcessorTableRow::new(&process_0, &process_1);
+        let row_1 = ProcessorTableRow::new(&process_1, &process_2);
+        let row_2 = ProcessorTableRow::new(&process_2, &process_3);
+        let row_3 = ProcessorTableRow::new(&process_3, &process_4);
+        let row_4 = ProcessorTableRow::new(&process_4, &process_5);
+        let row_5 = ProcessorTableRow::new(&process_5, &process_6);
+        let row_6 = ProcessorTableRow::new(&process_6, &process_7);
+        let row_7 = ProcessorTableRow::new(&process_7, &process_8);
+        let row_8 = ProcessorTableRow::new(&process_8, &process_9);
+        let row_9 = ProcessorTableRow::new(&process_9, &process_10);
+        let row_10 = ProcessorTableRow::new(&process_10, &process_11);
+        let row_11 = ProcessorTableRow::new(&process_11, &dummy_12);
+        let row_12 = ProcessorTableRow::new(&dummy_12, &dummy_13);
+        let row_13 = ProcessorTableRow::new(&dummy_13, &dummy_14);
+        let row_14 = ProcessorTableRow::new(&dummy_14, &dummy_15);
+        let row_15 = ProcessorTableRow::new(&dummy_15, &dummy_16);
+
+        expected_processor_table.add_row(row_0);
+        expected_processor_table.add_row(row_1);
+        expected_processor_table.add_row(row_2);
+        expected_processor_table.add_row(row_3);
+        expected_processor_table.add_row(row_4);
+        expected_processor_table.add_row(row_5);
+        expected_processor_table.add_row(row_6);
+        expected_processor_table.add_row(row_7);
+        expected_processor_table.add_row(row_8);
+        expected_processor_table.add_row(row_9);
+        expected_processor_table.add_row(row_10);
+        expected_processor_table.add_row(row_11);
+        expected_processor_table.add_row(row_12);
+        expected_processor_table.add_row(row_13);
+        expected_processor_table.add_row(row_14);
+        expected_processor_table.add_row(row_15);
 
         // Verify that the processor table is correct
+        assert_eq!(processor_intermediate_table, expected_processor_intermediate_table);
         assert_eq!(processor_table, expected_processor_table);
     }
 
     #[test]
     fn test_trace_evaluation_empty_processor_table() {
-        let processor_table = ProcessorIntermediateTable::new();
+        let processor_table = ProcessorTable::new();
         let result = processor_table.trace_evaluation();
 
         assert!(matches!(result, Err(TraceError::EmptyTrace)));
@@ -493,8 +742,8 @@ mod tests {
     #[test]
     #[allow(clippy::similar_names)]
     fn test_trace_evaluation_single_row_processor_table() {
-        let mut processor_table = ProcessorIntermediateTable::new();
-        processor_table.add_entry(ProcessorTableEntry {
+        let mut processor_intermediate_table = ProcessorIntermediateTable::new();
+        processor_intermediate_table.add_entry(ProcessorTableEntry {
             clk: BaseField::one(),
             ip: BaseField::from(2),
             ci: BaseField::from(3),
@@ -503,6 +752,8 @@ mod tests {
             mv: BaseField::from(6),
             mvi: BaseField::from(7),
         });
+
+        let processor_table = ProcessorTable::from(processor_intermediate_table);
 
         let (trace, claim) = processor_table.trace_evaluation().unwrap();
 
@@ -533,10 +784,10 @@ mod tests {
     #[test]
     #[allow(clippy::similar_names)]
     fn test_trace_evaluation_processor_table_with_multiple_rows() {
-        let mut processor_table = ProcessorIntermediateTable::new();
+        let mut processor_intermediate_table = ProcessorIntermediateTable::new();
 
-        // Add rows to the processor table
-        let rows = vec![
+        // Add entries to the processor table
+        let entries = vec![
             ProcessorTableEntry {
                 clk: BaseField::from(0),
                 ip: BaseField::from(1),
@@ -557,7 +808,9 @@ mod tests {
             },
         ];
 
-        processor_table.add_entries(rows);
+        processor_intermediate_table.add_entries(entries);
+
+        let processor_table = ProcessorTable::from(processor_intermediate_table);
 
         // Perform the trace evaluation
         let (trace, claim) = processor_table.trace_evaluation().unwrap();
