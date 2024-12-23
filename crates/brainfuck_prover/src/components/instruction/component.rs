b64f65a2b639c140fc2fe3c1704b7df5dcc72d67
malatrax <71888134+zmalatrax@users.noreply.github.com>
2024-12-23 16:31:12 +0100

    feat: add instruction component constraints (#166)
    * feat: add instruction component constraints
    
    * feat: add instruction component to entrypoints
    
    * chore: remove redundant clone
    
    * feat: add constraints on next_ci and next_ni

--- crates/brainfuck_prover/src/components/instruction/component.rs
+++ crates/brainfuck_prover/src/components/instruction/component.rs
@@ -1,8 +1,15 @@
 use super::table::InstructionElements;
 use crate::components::InstructionClaim;
+use num_traits::One;
 use stwo_prover::{
-    constraint_framework::{EvalAtRow, FrameworkComponent, FrameworkEval},
-    core::{channel::Channel, fields::qm31::SecureField},
+    constraint_framework::{
+        preprocessed_columns::PreprocessedColumn, EvalAtRow, FrameworkComponent, FrameworkEval,
+        RelationEntry,
+    },
+    core::{
+        channel::Channel,
+        fields::{m31::BaseField, qm31::SecureField},
+    },
 };
 
 /// Implementation of `Component` and `ComponentProver`
@@ -15,7 +22,7 @@ pub type InstructionComponent = FrameworkComponent<InstructionEval>;
 /// provided by the constraint framework of Stwo.
 pub struct InstructionEval {
     log_size: u32,
-    _instruction_lookup_elements: InstructionElements,
+    instruction_lookup_elements: InstructionElements,
 }
 
 impl InstructionEval {
@@ -23,7 +30,7 @@ impl InstructionEval {
         claim: &InstructionClaim,
         instruction_lookup_elements: InstructionElements,
     ) -> Self {
-        Self { log_size: claim.log_size, _instruction_lookup_elements: instruction_lookup_elements }
+        Self { log_size: claim.log_size, instruction_lookup_elements }
     }
 }
 
@@ -53,8 +60,70 @@ impl FrameworkEval for InstructionEval {
     /// Use `eval.add_to_relation` to define a global constraint for the logUp protocol.
     ///
     /// The logUp must be finalized with `eval.finalize_logup()`.
-    fn evaluate<E: EvalAtRow>(&self, _eval: E) -> E {
-        todo!()
+    #[allow(clippy::similar_names)]
+    fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
+        // Get the preprocessed column to check boundary constraints
+        let is_first = eval.get_preprocessed_column(PreprocessedColumn::IsFirst(self.log_size()));
+
+        // Get all the registers' columns
+        let ip = eval.next_trace_mask();
+        let ci = eval.next_trace_mask();
+        let ni = eval.next_trace_mask();
+        let d = eval.next_trace_mask();
+        let next_ip = eval.next_trace_mask();
+        let next_ci = eval.next_trace_mask();
+        let next_ni = eval.next_trace_mask();
+        let next_d = eval.next_trace_mask();
+
+        // Boundary constraints
+        eval.add_constraint(is_first * ip.clone());
+
+        // Consistency constraints
+        eval.add_constraint(d.clone() * (d.clone() - BaseField::one().into()));
+        eval.add_constraint(next_d.clone() * (next_d.clone() - BaseField::one().into()));
+
+        // If `d` is set, then `ci` equals 0
+        eval.add_constraint(d.clone() * ci.clone());
+
+        // If `d` is set, then `ni` equals 0
+        eval.add_constraint(d.clone() * ni.clone());
+
+        // If `next_d` is set, then `next_ci` equals 0
+        eval.add_constraint(next_d.clone() * next_ci.clone());
+
+        // If `next_d` is set, then `ni` equals 0
+        eval.add_constraint(next_d * next_ni.clone());
+
+        // Transition constraints
+
+        // `ip` increases by 0 or 1
+        eval.add_constraint(
+            (next_ip.clone() - ip.clone()) *
+                (next_ip.clone() - ip.clone() - BaseField::one().into()),
+        );
+
+        // If `ip` remains the same, then `ci` remains the same
+        eval.add_constraint(
+            (next_ip.clone() - ip.clone() - BaseField::one().into()) * (next_ci - ci.clone()),
+        );
+
+        // If `ip` remains the same, then `ni` remains the same
+        eval.add_constraint(
+            (next_ip - ip.clone() - BaseField::one().into()) * (next_ni - ni.clone()),
+        );
+
+        // Interaction constraints
+
+        let num = d - E::F::one();
+        eval.add_to_relation(&[RelationEntry::new(
+            &self.instruction_lookup_elements,
+            num.into(),
+            &[ip, ci, ni],
+        )]);
+
+        eval.finalize_logup();
+
+        eval
     }
 }
 
@@ -79,5 +148,73 @@ impl InteractionClaim {
     /// to bound the proof to the trace.
     pub fn mix_into(&self, channel: &mut impl Channel) {
         channel.mix_felts(&[self.claimed_sum]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use brainfuck_vm::{compiler::Compiler, test_helper::create_test_machine};
+    use stwo_prover::{
+        constraint_framework::{
+            assert_constraints, preprocessed_columns::gen_is_first, FrameworkEval,
+        },
+        core::{
+            pcs::TreeVec,
+            poly::circle::{CanonicCoset, CircleEvaluation},
+        },
+    };
+
+    use crate::components::instruction::{
+        component::InstructionEval,
+        table::{interaction_trace_evaluation, InstructionElements, InstructionTable},
+    };
+
+    #[test]
+    fn test_instruction_constraints() {
+        const LOG_SIZE: u32 = 9;
+
+        // Get an execution trace from a valid Brainfuck program
+        let code = "+>,<[>+.<-]";
+        let mut compiler = Compiler::new(code);
+        let instructions = compiler.compile();
+        let (mut machine, _) = create_test_machine(&instructions, &[1]);
+        let () = machine.execute().expect("Failed to execute machine");
+
+        let trace_vm = machine.trace();
+
+        // Construct the IsFirst preprocessed column
+        let is_first_col = gen_is_first(LOG_SIZE);
+        let is_first_col_2 = gen_is_first(LOG_SIZE);
+        let preprocessed_trace = vec![is_first_col, is_first_col_2];
+
+        // Construct the main trace from the execution trace
+        let table = InstructionTable::from((&trace_vm, machine.program()));
+        let (instruction_trace, claim) = table.trace_evaluation().unwrap();
+
+        // Draw Interaction elements
+        let instruction_lookup_elements = InstructionElements::dummy();
+
+        // Generate interaction trace
+        let (interaction_trace, interaction_claim) =
+            interaction_trace_evaluation(&instruction_trace, &instruction_lookup_elements).unwrap();
+
+        // Create the trace evaluation TreeVec
+        let trace = TreeVec::new(vec![preprocessed_trace, instruction_trace, interaction_trace]);
+
+        // Interpolate the trace for the evaluation
+        let trace_polys = trace.map_cols(CircleEvaluation::interpolate);
+
+        // Get the Instruction AIR evaluator
+        let instruction_eval = InstructionEval::new(&claim, instruction_lookup_elements);
+
+        // Assert that the constraints are valid for a valid Brainfuck program.
+        assert_constraints(
+            &trace_polys,
+            CanonicCoset::new(LOG_SIZE),
+            |eval| {
+                instruction_eval.evaluate(eval);
+            },
+            (interaction_claim.claimed_sum, None),
+        );
     }
 }
