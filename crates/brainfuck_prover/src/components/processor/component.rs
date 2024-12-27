933c0e562019c02a983cd7800cd74023f122db33
malatrax <simon.malatrait@grenoble-inp.org>
2024-12-27 14:59:38 +0100

    feat: add instruction specific constraints to Processor component

--- crates/brainfuck_prover/src/components/processor/component.rs
+++ crates/brainfuck_prover/src/components/processor/component.rs
@@ -2,13 +2,17 @@ use crate::components::{
     instruction::table::InstructionElements, io::table::IoElements, memory::table::MemoryElements,
     ProcessorClaim,
 };
+use brainfuck_vm::instruction::{InstructionType, VALID_INSTRUCTIONS_BF};
 use num_traits::One;
 use stwo_prover::{
     constraint_framework::{
         preprocessed_columns::PreprocessedColumn, EvalAtRow, FrameworkComponent, FrameworkEval,
         RelationEntry,
     },
-    core::{channel::Channel, fields::qm31::SecureField},
+    core::{
+        channel::Channel,
+        fields::{m31::BaseField, qm31::SecureField},
+    },
 };
 
 /// Implementation of `Component` and `ComponentProver`
@@ -43,6 +47,35 @@ impl ProcessorEval {
             instruction_lookup_elements,
         }
     }
+}
+
+/// Computes the value of the deselector polynomial for a given `ci` value from the trace
+/// and the wanted instruction.
+///
+/// This polynomial allows to conditionally render AIR for the various instructions.
+///
+/// # Arguments
+/// * ci - The evaluated value on which the deselector polynomial is evaluated upon.
+/// * instruction - The type of instruction which
+///
+/// # Returns
+/// It returns zero when `ci` is 0 or different from the available instructions except one.
+/// If `ci` equals the `instruction`, then it returns something non-zero.
+pub fn deselector_polynomial<E: EvalAtRow>(ci: &E::F, instruction: &InstructionType) -> E::F {
+    VALID_INSTRUCTIONS_BF
+        .iter()
+        .filter(|&&val| val != instruction.to_base_field())
+        .fold(ci.clone(), |acc, &val| acc * (ci.clone() - val.into()))
+}
+
+#[cfg(test)]
+/// Equivalent of `deselector_polynomial` with the [`BaseField`],
+/// used to easily test the logic.
+pub fn deselector_poly(ci: &BaseField, instruction: &InstructionType) -> BaseField {
+    VALID_INSTRUCTIONS_BF
+        .iter()
+        .filter(|&&val| val != instruction.to_base_field())
+        .fold(*ci, |acc, &val| acc * (*ci - val))
 }
 
 impl FrameworkEval for ProcessorEval {
@@ -87,13 +120,27 @@ impl FrameworkEval for ProcessorEval {
         let mvi = eval.next_trace_mask();
         let d = eval.next_trace_mask();
         let next_clk = eval.next_trace_mask();
-        let _next_ip = eval.next_trace_mask();
+        let next_ip = eval.next_trace_mask();
         let _next_ci = eval.next_trace_mask();
         let _next_ni = eval.next_trace_mask();
         let next_mp = eval.next_trace_mask();
         let next_mv = eval.next_trace_mask();
         let _next_mvi = eval.next_trace_mask();
         let next_d = eval.next_trace_mask();
+
+        // TODO: Huge inefficiency (implies constraints of degree at least 8...)
+        // TODO: Remove this deselector by splitting all instructions into their own components.
+        let deselector_jump_if_zero = deselector_polynomial::<E>(&ci, &InstructionType::JumpIfZero);
+        let deselector_jump_if_not_zero =
+            deselector_polynomial::<E>(&ci, &InstructionType::JumpIfNotZero);
+        let deselector_left = deselector_polynomial::<E>(&ci, &InstructionType::Left);
+        let deselector_right = deselector_polynomial::<E>(&ci, &InstructionType::Right);
+        let deselector_minus = deselector_polynomial::<E>(&ci, &InstructionType::Minus);
+        let deselector_plus = deselector_polynomial::<E>(&ci, &InstructionType::Plus);
+        let deselector_input = deselector_polynomial::<E>(&ci, &InstructionType::ReadChar);
+        let deselector_output = deselector_polynomial::<E>(&ci, &InstructionType::PutChar);
+
+        let is_mv_zero = E::F::one() - mv.clone() * mvi.clone();
 
         // Boundary constraints
         // `clk` starts at 0
@@ -109,12 +156,82 @@ impl FrameworkEval for ProcessorEval {
         // `mv` is the inverse of `mvi`
         eval.add_constraint(mv.clone() * (mv.clone() * mvi.clone() - E::F::one()));
         // `mvi` is the inverse of `mv`
-        eval.add_constraint(mvi.clone() * (mv.clone() * mvi - E::F::one()));
+        eval.add_constraint(mvi.clone() * (mv.clone() * mvi.clone() - E::F::one()));
 
         // Transition constraints
 
         // `clk` increases by 1
         eval.add_constraint(next_clk.clone() - clk.clone() - E::F::one());
+
+        // JumpIfZero - [
+        // Jump to `ni` if `mv = 0`. Otherwise, skip two.
+        eval.add_constraint(
+            deselector_jump_if_zero.clone() *
+                (mvi.clone() * (next_ip.clone() - ip.clone() - BaseField::from(2).into()) +
+                    is_mv_zero.clone() * (next_ip.clone() - ni.clone())),
+        );
+        // `mp` remains the same
+        eval.add_constraint(deselector_jump_if_zero.clone() * (next_mp.clone() - mp.clone()));
+        // `mv` remains the same
+        eval.add_constraint(deselector_jump_if_zero * (next_mv.clone() - mv.clone()));
+
+        // JumpIfNotZero - ]
+        // Jump to `ni` if `mv != 0`. Otherwise, skip two.
+        eval.add_constraint(
+            deselector_jump_if_not_zero.clone() *
+                (is_mv_zero * (next_ip.clone() - ip.clone() - BaseField::from(2).into()) +
+                    mvi * (next_ip.clone() - ni.clone())),
+        );
+        // `mp` remains the same
+        eval.add_constraint(deselector_jump_if_not_zero.clone() * (next_mp.clone() - mp.clone())); // `mv` remains the same
+        eval.add_constraint(deselector_jump_if_not_zero * (next_mv.clone() - mv.clone()));
+
+        // ShiftLeft - <
+
+        // `ip` increases by one
+        eval.add_constraint(deselector_left.clone() * (next_ip.clone() - ip.clone() - E::F::one())); // `mp` decreases by one
+        eval.add_constraint(deselector_left * (next_mp.clone() - mp.clone() + E::F::one()));
+
+        // ShiftRight - >
+        // `ip` increases by one
+        eval.add_constraint(
+            deselector_right.clone() * (next_ip.clone() - ip.clone() - E::F::one()),
+        );
+        // `mp` increases by one
+        eval.add_constraint(deselector_right * (next_mp.clone() - mp.clone() - E::F::one()));
+
+        // MemoryDecrement - -
+        // `ip` increases by one
+        eval.add_constraint(
+            deselector_minus.clone() * (next_ip.clone() - ip.clone() - E::F::one()),
+        );
+        // `mp` remains the same
+        eval.add_constraint(deselector_minus.clone() * (next_mp.clone() - mp.clone()));
+        // `mv` decreases by one
+        eval.add_constraint(deselector_minus * (next_mv.clone() - mv.clone() + E::F::one()));
+
+        // MemoryIncrement - +
+        // `ip` increases by one
+        eval.add_constraint(deselector_plus.clone() * (next_ip.clone() - ip.clone() - E::F::one())); // `mp` remains the same
+        eval.add_constraint(deselector_plus.clone() * (next_mp.clone() - mp.clone()));
+        // `mv` increases by one
+        eval.add_constraint(deselector_plus * (next_mv.clone() - mv.clone() - E::F::one()));
+
+        // Input - ,
+        // `ip` increases by one
+        eval.add_constraint(
+            deselector_input.clone() * (next_ip.clone() - ip.clone() - E::F::one()),
+        );
+        // `mp` remains the same
+        eval.add_constraint(deselector_input * (next_mp.clone() - mp.clone()));
+
+        // Output - .
+        // `ip` increases by one
+        eval.add_constraint(deselector_output.clone() * (next_ip - ip.clone() - E::F::one()));
+        // `mp` remains the same
+        eval.add_constraint(deselector_output.clone() * (next_mp.clone() - mp.clone()));
+        // `mv` remains the same
+        eval.add_constraint(deselector_output * (next_mv.clone() - mv.clone()));
 
         // Lookup arguments
 
@@ -181,6 +298,7 @@ impl InteractionClaim {
 
 #[cfg(test)]
 mod tests {
+    use super::deselector_poly;
     use crate::components::{
         instruction::table::InstructionElements,
         io::table::IoElements,
@@ -190,12 +308,16 @@ mod tests {
             table::{interaction_trace_evaluation, ProcessorTable},
         },
     };
-    use brainfuck_vm::{compiler::Compiler, test_helper::create_test_machine};
+    use brainfuck_vm::{
+        compiler::Compiler, instruction::InstructionType, test_helper::create_test_machine,
+    };
+    use num_traits::Zero;
     use stwo_prover::{
         constraint_framework::{
             assert_constraints, preprocessed_columns::gen_is_first, FrameworkEval,
         },
         core::{
+            fields::m31::BaseField,
             pcs::TreeVec,
             poly::circle::{CanonicCoset, CircleEvaluation},
         },
@@ -263,5 +385,133 @@ mod tests {
             },
             (interaction_claim.claimed_sum, None),
         );
+    }
+
+    #[test]
+    fn deselector_jump_if_zero_valid() {
+        let ci = InstructionType::JumpIfZero.to_base_field();
+        let deselector_jump_if_zero = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_ne!(deselector_jump_if_zero, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_jump_if_zero_invalid() {
+        let ci = InstructionType::JumpIfZero.to_base_field();
+        let deselector_jump_if_zero = deselector_poly(&ci, &InstructionType::Plus);
+
+        assert_eq!(deselector_jump_if_zero, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_jump_if_not_zero_valid() {
+        let ci = InstructionType::JumpIfNotZero.to_base_field();
+        let deselector_jump_if_not_zero = deselector_poly(&ci, &InstructionType::JumpIfNotZero);
+
+        assert_ne!(deselector_jump_if_not_zero, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_jump_if_not_zero_invalid() {
+        let ci = InstructionType::JumpIfNotZero.to_base_field();
+        let deselector_jump_if_not_zero = deselector_poly(&ci, &InstructionType::Left);
+
+        assert_eq!(deselector_jump_if_not_zero, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_left_valid() {
+        let ci = InstructionType::Left.to_base_field();
+        let deselector_left = deselector_poly(&ci, &InstructionType::Left);
+
+        assert_ne!(deselector_left, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_left_invalid() {
+        let ci = InstructionType::Left.to_base_field();
+        let deselector_left = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_left, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_right_valid() {
+        let ci = InstructionType::Right.to_base_field();
+        let deselector_right = deselector_poly(&ci, &InstructionType::Right);
+
+        assert_ne!(deselector_right, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_right_invalid() {
+        let ci = InstructionType::Right.to_base_field();
+        let deselector_right = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_right, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_minus_valid() {
+        let ci = InstructionType::Minus.to_base_field();
+        let deselector_minus = deselector_poly(&ci, &InstructionType::Minus);
+
+        assert_ne!(deselector_minus, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_minus_invalid() {
+        let ci = InstructionType::Minus.to_base_field();
+        let deselector_minus = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_minus, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_plus_valid() {
+        let ci = InstructionType::Plus.to_base_field();
+        let deselector_plus = deselector_poly(&ci, &InstructionType::Plus);
+
+        assert_ne!(deselector_plus, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_plus_invalid() {
+        let ci = InstructionType::Plus.to_base_field();
+        let deselector_plus = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_plus, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_input_valid() {
+        let ci = InstructionType::ReadChar.to_base_field();
+        let deselector_input = deselector_poly(&ci, &InstructionType::ReadChar);
+
+        assert_ne!(deselector_input, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_input_invalid() {
+        let ci = InstructionType::ReadChar.to_base_field();
+        let deselector_input = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_input, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_output_valid() {
+        let ci = InstructionType::PutChar.to_base_field();
+        let deselector_output = deselector_poly(&ci, &InstructionType::PutChar);
+
+        assert_ne!(deselector_output, BaseField::zero());
+    }
+
+    #[test]
+    fn deselector_output_invalid() {
+        let ci = InstructionType::PutChar.to_base_field();
+        let deselector_output = deselector_poly(&ci, &InstructionType::JumpIfZero);
+
+        assert_eq!(deselector_output, BaseField::zero());
     }
 }
